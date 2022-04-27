0b347926d3f1405835ee5cba28309ed3b97d9930
unknown <subashsci01@gmail.com>
2022-04-27 08:50:15 +0530

    First commit on app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var Addition = require('./models/Addition.js');
+console.log('Adition of two numbers : ' + Addition.addTwoNumbers(2,2));
+
+var Multiply = require('./models/Multiply.js');
+console.log('Multiplication of two numbers : ' + Multiply.multiplyTwoNumbers(2,2));
+
+var Subtraction = require('./models/Subtraction.js');
+console.log('Subtract two numbers : '+ Subtraction.subtractTwoNumbers(2,2));
+
+var Division = require('./models/Division.js');
+console.log('Division of two numbers : '+ Division.dividingTwoNumbers(2,2));
